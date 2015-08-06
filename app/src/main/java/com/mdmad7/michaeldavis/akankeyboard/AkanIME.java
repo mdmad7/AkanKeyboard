0dbcd0216f11f8553dcb6cc74ea75c9d33fde409
Michael <mdmad7@gmail.com>
2015-08-06 12:21:50 +0000

    More Functional than initial release

--- app/src/main/java/com/mdmad7/michaeldavis/akankeyboard/AkanIME.java
+++ app/src/main/java/com/mdmad7/michaeldavis/akankeyboard/AkanIME.java
@@ -5,11 +5,13 @@ import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
 import android.text.Editable;
+import android.text.InputType;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodSubtype;
 import android.widget.EditText;
 
 /**
@@ -24,8 +26,12 @@ public class AkanIME extends InputMethodService
     private Keyboard numPadSymbols;
     private Keyboard simpleNumpad;
     private Keyboard qwertyGoKeyboard;
+    private Keyboard qwertyGoSymbolsKeyboard;
+    private Keyboard qwertyEmKeyboard;
+    private Keyboard qwertyEmSymbolKeyboard;
+    private Keyboard mCurKeyboard;
+    private CandidateView mCandidateView;
 
-    public static final int codeSearch = 55000;
 
     private boolean caps = false; //caps lock
 
@@ -38,6 +44,11 @@ public class AkanIME extends InputMethodService
         numPadSymbols = new Keyboard(this, R.xml.numpad_symbols);
         simpleNumpad = new Keyboard(this, R.xml.simple_numpad);
         qwertyGoKeyboard = new Keyboard(this, R.xml.qwerty_go);
+        qwertyGoSymbolsKeyboard = new Keyboard(this, R.xml.qwerty_go_symbols);
+        qwertyEmKeyboard = new Keyboard(this, R.xml.qwerty_email);
+        qwertyEmSymbolKeyboard = new Keyboard(this, R.xml.qwerty_em_symbols);
+
+
 
 
         keyboardView.setKeyboard(qwertyKeyboard);
@@ -63,17 +74,63 @@ public class AkanIME extends InputMethodService
         }
     }
 
-    @Override
-    public void onStartInputView(EditorInfo info, boolean restarting) {
-        super.onStartInputView(info, restarting);
-
-//      Sets keyboard defaults whenever keyboard is reloaded
-        keyboardView.setKeyboard(qwertyKeyboard);
+    @Override public View onCreateCandidatesView() {
+        mCandidateView = new CandidateView(this);
+        mCandidateView.setService(this);
+        return mCandidateView;
     }
 
     @Override
     public void onStartInput(EditorInfo attribute, boolean restarting) {
         super.onStartInput(attribute, restarting);
+
+    }
+
+    @Override
+    public void onStartInputView(EditorInfo attribute, boolean restarting) {
+        super.onStartInputView(attribute, restarting);
+
+//      Sets keyboard defaults whenever keyboard is reloaded
+        keyboardView.setKeyboard(qwertyKeyboard);
+
+        // We are now going to initialize our state based on the type of
+        // text being edited.
+        switch (attribute.inputType & InputType.TYPE_MASK_CLASS) {
+            case InputType.TYPE_CLASS_NUMBER:
+            case InputType.TYPE_CLASS_DATETIME:
+                // Numbers and dates default to the simple numberpad, with
+                // no extra features.
+                mCurKeyboard = simpleNumpad;
+                break;
+
+            case InputType.TYPE_CLASS_PHONE:
+                // Phones will also default to the phone number Pad
+                mCurKeyboard = numPad;
+                break;
+
+            case InputType.TYPE_CLASS_TEXT:
+                // This is general text editing.  We will default to the
+                // normal alphabetic keyboard.
+                mCurKeyboard = qwertyKeyboard;
+
+                int variation = attribute.inputType & InputType.TYPE_MASK_VARIATION;
+                if (variation == InputType.TYPE_TEXT_VARIATION_URI
+                        || variation == InputType.TYPE_TEXT_VARIATION_FILTER) {
+
+                    mCurKeyboard = qwertyGoKeyboard;
+                }else if(variation == InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS){
+                    mCurKeyboard = qwertyEmKeyboard;
+                }
+                break;
+
+            default:
+                // For all unknown input types, default to the alphabetic
+                // keyboard with no special features.
+                mCurKeyboard = qwertyKeyboard;
+        }
+
+        keyboardView.setKeyboard(mCurKeyboard);
+        keyboardView.closing();
 
     }
 
@@ -103,7 +160,7 @@ public class AkanIME extends InputMethodService
                 }
                 break;
 
-                //  Matches SYM Key switches for all keyboards with respective symbol keyboard
+                //  Matches SYM Key switches to all keyboards with respective symbol keyboard
             case Keyboard.KEYCODE_MODE_CHANGE:
                 Keyboard current = keyboardView.getKeyboard();
                 if (current == qwertySymbolsKeyboard) {
@@ -111,29 +168,30 @@ public class AkanIME extends InputMethodService
                 }else if(current == qwertyKeyboard){
                     keyboardView.setKeyboard(qwertySymbolsKeyboard);
                 }else if(current == qwertyGoKeyboard) {
-            keyboardView.setKeyboard(qwertySymbolsKeyboard);
-               }else if (current == numPadSymbols){
+                    keyboardView.setKeyboard(qwertyGoSymbolsKeyboard);
+               }else if(current == qwertyGoSymbolsKeyboard){
+                    keyboardView.setKeyboard(qwertyGoKeyboard);
+                }else if (current == numPadSymbols){
                     keyboardView.setKeyboard(numPad);
-                }
-                else if(current == numPad){
+                }else if (current == qwertyEmSymbolKeyboard) {
+                    keyboardView.setKeyboard(qwertyEmKeyboard);
+                }else if (current == qwertyEmKeyboard) {
+                    keyboardView.setKeyboard(qwertyEmSymbolKeyboard);
+                }else if(current == numPad){
                     keyboardView.setKeyboard(numPadSymbols);
                 }
                 break;
-
-//            case 46:
-//                ic.commitText(String.valueOf(code), 1);
-//                if(ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_SPACE))){
-//                    caps =!caps;
-//                    keyboardView.getKeyboard().setShifted(caps);
-//                    keyboardView.invalidateAllKeys();
-//                }
-//
-//                break;
+            case 55001:
+                ic.commitText(".com", 1);
+                break;
 
             case 32:
+                //allows symbol keyboard to revert to ABC keyboard when spacebar is pressed.
                 ic.commitText(String.valueOf(code), 1);
-                if(keyboardView.getKeyboard().setShifted(caps)){
-                    keyboardView.invalidateAllKeys();
+                if (keyboardView.getKeyboard() == qwertySymbolsKeyboard) {
+                    keyboardView.setKeyboard(qwertyKeyboard);
+                }else if(keyboardView.getKeyboard() == qwertyGoSymbolsKeyboard){
+                    keyboardView.setKeyboard(qwertyGoKeyboard);
                 }
                 break;
 
